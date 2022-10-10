507a283935f26b93a8528062ebfc8ecf300e9249
Arilex <110915525+Xaisar@users.noreply.github.com>
2022-10-10 10:58:12 +0700

    Arilex

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,9 +1,8 @@
 import 'package:digital_sampah/app/routes/app_pages.dart';
-import 'package:flutter/gestures.dart';
+// import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
 
 import '../controllers/login_controller.dart';
 
@@ -11,208 +10,124 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: ListView(
-          children: [
-            SizedBox(height: 20),
-            Container(
-              alignment: Alignment.centerLeft,
-              height: 150,
-              child: Image.asset(
-                "assets/logo/logo-login.png",
-                fit: BoxFit.contain,
-              ),
-            ),
-            SizedBox(height: 30),
-            Text(
-              "Silahkan masuk dengan nomor telkomsel kamu",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30),
-            Text(
-              "Nomor HP",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: controller.phoneC,
-              keyboardType: TextInputType.phone,
-              autocorrect: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Cth. 08129011xxxx",
-              ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Obx(
-                  () => Checkbox(
-                    activeColor: Colors.red,
-                    value: controller.checkC.value,
-                    onChanged: (value) => controller.checkC.toggle(),
-                  ),
-                ),
-                Expanded(
-                  child: RichText(
-                    text: TextSpan(
-                      text: "Saya menyetujui ",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                      children: [
-                        TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print("klik syarat");
-                            },
-                          text: "syarat",
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                        TextSpan(
-                          text: ",",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print("klik ketentuan");
-                            },
-                          text: "ketentuan",
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                        TextSpan(
-                          text: ", dan ",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print("klik privasi");
-                            },
-                          text: "privasi",
-                          style: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                        TextSpan(
-                          text: " Telkomsel",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () => Get.offAllNamed(Routes.HOME),
-              child: Text(
-                "MASUK",
-                style: TextStyle(
-                  color: Color(0xFF747D8C),
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.grey[300],
-                fixedSize: Size(150, 50),
-              ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: Text("Atau masuk menggunakan"),
-            ),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset("assets/icons/fb.png"),
-                      SizedBox(width: 7),
-                      Text(
-                        "Facebook",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xFF3B5998),
-                        ),
-                      ),
-                    ],
-                  ),
-                  style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(
-                      Size(150, 50),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                        color: Color(0xFF3B5998),
-                      ),
-                    ),
-                  ),
-                ),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset("assets/icons/twitter.png"),
-                      SizedBox(width: 7),
-                      Text(
-                        "Twitter",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xFF1DA1F2),
-                        ),
-                      ),
-                    ],
-                  ),
-                  style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(
-                      Size(150, 50),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                        color: Color(0xFF1DA1F2),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
+      backgroundColor: Color(0xff1AD443),
+      body: Column(children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 50),
+          alignment: Alignment.topCenter,
+          // color: Colors.black,
+          child: SizedBox(
+            width: Get.width * 0.4,
+            height: Get.width * 0.4,
+            child: Image.asset("assets/logo/logo1.png"),
+          ),
         ),
-      ),
+        Expanded(
+          child: Container(
+            decoration: const BoxDecoration(
+              boxShadow: [BoxShadow(color: Colors.white, blurRadius: 5)],
+              color: Colors.white,
+              borderRadius:
+                  BorderRadiusDirectional.only(topStart: Radius.circular(80)),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.topCenter,
+                  margin: const EdgeInsets.only(
+                    top: 30,
+                  ),
+                  child: const Text(
+                    'Selamat Datang',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topCenter,
+                  margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+                  // color: Colors.red,
+                  child: Column(children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: const EdgeInsets.only(bottom: 20),
+                      child: Column(
+                        children: [
+                          Container(
+                              margin: const EdgeInsets.only(bottom: 15),
+                              child: Column(children: [
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  margin: const EdgeInsets.only(bottom: 10),
+                                  child: const Text(
+                                    'Username',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                                const TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Input Username',
+                                    focusColor: Colors.grey,
+                                  ),
+                                ),
+                              ])),
+                          Container(
+                              margin: const EdgeInsets.only(bottom: 15),
+                              child: Column(children: [
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  margin: const EdgeInsets.only(bottom: 10),
+                                  child: const Text(
+                                    'Password',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                                const TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Input Password',
+                                    focusColor: Colors.grey,
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.centerRight,
+                                  margin: const EdgeInsets.only(top: 5),
+                                  child: TextButton(
+                                    style: TextButton.styleFrom(
+                                      textStyle: const TextStyle(fontSize: 14),
+                                    ),
+                                    onPressed: () {},
+                                    child: const Text('Lupa Password?'),
+                                  ),
+                                ),
+                              ])),
+                          SizedBox(
+                            width: 150,
+                            height: 60,
+                            child: ElevatedButton(
+                              style: const ButtonStyle(),
+                              onPressed: () => Get.offAllNamed(Routes.HOME),
+                              child: const Text(
+                                'Masuk',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ]),
+                )
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
