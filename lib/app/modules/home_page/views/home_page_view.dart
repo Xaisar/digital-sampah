dc0dbc9919471aefc9dda3e6168b5e7557753b43
caritahu14 <ajihermansya14@gmail.com>
2022-10-11 14:28:25 +0700

    baru.v4

--- lib/app/modules/home_page/views/home_page_view.dart
+++ lib/app/modules/home_page/views/home_page_view.dart
@@ -1,8 +1,14 @@
-import 'package:digital_sampah/app/modules/home/views/home_view.dart';
 import 'package:digital_sampah/component/card_title.dart';
+import 'package:digital_sampah/component/my_button.dart';
+import 'package:digital_sampah/component/my_card.dart';
+import 'package:digital_sampah/component/my_listtile.dart';
+import 'package:digital_sampah/models/news.dart';
+import 'package:digital_sampah/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../controllers/home_page_controller.dart';
 
@@ -14,126 +20,230 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //page controller
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-         //nama aplikasi
-        centerTitle: false,
-        title: RichText(
-          text: TextSpan(
-            text: "Hai, ",
-            style: TextStyle(
-              fontSize: 18,
-            ),
-            children: [
-              TextSpan(
-                text: "Mohamad Aji Hermansya",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          // GestureDetector(
-          //   onTap: () {},
-          //   child: Container(
-          //       margin: EdgeInsets.only(right: 15),
-          //       width: 30,
-          //       height: 30,
-          //       child: Image.asset(
-          //         "assets/icons/profile.png",
-          //         fit: BoxFit.contain,
-          //       )), 
-          // ),
-          Container(
-                margin: EdgeInsets.only(right: 15),
-                width: 30,
-                height: 30,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.all(1),
-                        primary: Color(0xFF1AD443),
-                        shape: new CircleBorder(),
+      backgroundColor: Color(0xFFEBF9EE),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //appbarr
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "My",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      onPressed: () {},
-                      child: Icon(Icons.person_outlined),
+                      Text(
+                        "Cards",
+                        style: TextStyle(fontSize: 28),
+                      ),
+                    ],
+                  ),
+                  //plus butto
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(4),
+                      primary: Color(0xFFEBF9EE),
+                      shape: new CircleBorder(),
                     ),
-          ),
-        ],
-        backgroundColor: Color(0xFF1AD443),
-        elevation: 0,
-      ),
-  //clipPath
-      body: Stack(
-        children: [
-          ClipPath(
-            clipper: ClipPathClass(),
-            child: Container(
-              height: 250,
-              width: Get.width,
-              color: Color(0xFF1AD443),
+                    onPressed: () {},
+                    child: Icon(Icons.logout),
+                  ),
+                ],
+              ),
             ),
-          ),
-        //kartu
+            SizedBox(height: 25),
+            //cards
+            Container(
+              height: 200,
+              child: PageView(
+                scrollDirection: Axis.horizontal,
+                controller: _controller,
+                children: [
+                  MyCards(
+                    balance: 258543,
+                    cardNumber: 3213125205680006,
+                    Dawis1: "Dawis001",
+                    color: Colors.deepPurple[300],
+                  ),
+                  MyCard(
+                      color1: Colors.green[400],
+                      Nama: "Mohamad Aji Hermansya",
+                      User: "Anggota",
+                      Ttl: "Banyuwangi, 23 Februari 2004",
+                      Alamat: "Dusun Tambong",
+                      TitleHariPenjemputan: "Hari Juma't",
+                      TanggalJoin: "23/01/2010"),
+                ],
+              ),
+            ),
+            SizedBox(height: 25),
+            SmoothPageIndicator(
+              controller: _controller,
+              count: 2,
+              effect: ExpandingDotsEffect(activeDotColor: Colors.grey.shade800),
+            ),
+            SizedBox(height: 20),
+            //3 buttons -> send + pay +bill
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //sendbutton
+                  MyButton(
+                    iconImagePath: "assets/icons/send.png",
+                    butoonText: "Rupiah",
+                  ),
+                  //paybutton
+                  MyButton(
+                    iconImagePath: "assets/icons/credit-card.png",
+                    butoonText: "Riwayat",
+                  ),
+                  //bills button
+                  MyButton(
+                    iconImagePath: "assets/icons/bill.png",
+                    butoonText: "PLN",
+                  ),
+                ],
+              ),
+            ),
 
-        Container(
-          child: PageView(
-            children: [
-              MyCard(NIK_Nasabah: 3213125205680006, 
-              NoDawis: 001, 
-              TitleSaldo: 156000, 
-              TitleHariPenjemputan: "Hari Juma't", 
-              color: Color(0xffF7EC09), 
-              TitleAnggota: "active")
-            ],
-          ),
+            SizedBox(height: 13),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //sendbutton
+                  MyButton(
+                    iconImagePath: "assets/icons/send.png",
+                    butoonText: "Air",
+                  ),
+                  //paybutton
+                  MyButton(
+                    iconImagePath: "assets/icons/credit-card.png",
+                    butoonText: "PBB",
+                  ),
+                  //bills button
+                  MyButton(
+                    iconImagePath: "assets/icons/bill.png",
+                    butoonText: "Bantuan",
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            //column -> stats + transaction
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  //statistics
+                  MyListTile(
+                    iconImagePath: "assets/icons/statistics.png",
+                    tileName: "Statistics",
+                    tileSubName: "Payments and Icome",
+                  ),
+                  //transaction
+                  SizedBox(height: 10),
+                  MyListTile(
+                    iconImagePath: "assets/icons/mobile-transfer.png",
+                    tileName: "Transactions",
+                    tileSubName: "Transaction History",
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  //GopayLater
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 32),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/gopaylater.png',
+                          height: 14,
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'Lebih hemat pake GoPayLater 🤩',
+                          // style: bold16.copyWith(color: dark1),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'Yuk, belanja di Tokopedia pake GoPay Later dan nikmatin cashback-nya~',
+                          // style: regular14.copyWith(color: dark2),
+                        )
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 24,
+                  ),
+
+                  //News
+                  // ...News.map((item) => Padding(
+                  //         padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16),
+                  //         child: Container(
+                  //           clipBehavior: Clip.hardEdge,
+                  //           decoration: BoxDecoration(
+                  //               borderRadius: BorderRadius.circular(15),
+                  //               border: Border.all(color: dark4)),
+                  //           child: Column(
+                  //             children: [
+                  //               Image.asset('assets/images/${item.image}'),
+                  //               Padding(
+                  //                 padding: const EdgeInsets.all(24),
+                  //                 child: Column(
+                  //                   crossAxisAlignment: CrossAxisAlignment.start,
+                  //                   children: [
+                  //                     Text(
+                  //                       item.title,
+                  //                       style: bold16.copyWith(color: dark1),
+                  //                     ),
+                  //                     const SizedBox(
+                  //                       height: 8,
+                  //                     ),
+                  //                     Text(
+                  //                       item.description,
+                  //                       style: regular14.copyWith(color: dark2),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //               )
+                  //             ],
+                  //           ),
+                  //         ),
+                  //       ),),
+                ],
+              ),
+            ),
+          ],
         ),
-
-        
-
-
-
-        ],
-
       ),
-
-
-
-
-    
-
-        
-
-
-
-          //menu
-
-          //informasipenjemputan
-
-          //informasievent
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     );
   }
 }
---
+++ lib/component/my_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class MyCards extends StatelessWidget {
+  final double balance;
+  final int cardNumber;
+  final String Dawis1;
+  final color;
+
+  const MyCards({
+    super.key,
+    required this.balance,
+    required this.cardNumber,
+    required this.Dawis1,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Container(
+        width: 300,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Total Saldo",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                Image.asset(
+                  "assets/icons/visa.png",
+                  height: 60,
+                ),
+              ],
+            ),
+             Text(
+              "\RP " + balance.toString(),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //cardinformation
+                Text(
+                  cardNumber.toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                //cardexpiry
+                Text(
+                  Dawis1,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
