d2a6168dd1728d5ef691fc451bf0f7624b80440a
Arilex <110915525+Xaisar@users.noreply.github.com>
2022-10-13 20:11:05 +0700

    arilex

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -9,17 +9,16 @@ abstract class Routes {
   static const HOME_PAGE = _Paths.HOME_PAGE;
   static const HOME_PETUGAS = _Paths.HOME_PETUGAS;
   static const LUPA_PASSWORD = _Paths.LUPA_PASSWORD;
-static const HARGA_SAMPAH = _Paths.HARGA_SAMPAH;
+  static const HARGA_SAMPAH = _Paths.HARGA_SAMPAH;
   static const RIWAYAT = _Paths.RIWAYAT;
   static const TRANSAKSI = _Paths.TRANSAKSI;
   static const JADWAL = _Paths.JADWAL;
   static const PENGAMBILAN = _Paths.PENGAMBILAN;
   static const BANTUAN = _Paths.BANTUAN;
   static const STATISTIKNASABAH = _Paths.STATISTIKNASABAH;
-    static const PEMAYARAN = _Paths.PEMAYARAN;
+  static const PEMAYARAN = _Paths.PEMAYARAN;
   static const PAYMENTPAGE = _Paths.PAYMENTPAGE;
 }
-
 
 abstract class _Paths {
   static const HOME = '/home';
@@ -27,14 +26,13 @@ abstract class _Paths {
   static const HOME_PAGE = '/home-page';
   static const HOME_PETUGAS = '/home-petugas';
   static const LUPA_PASSWORD = '/lupa-password';
-   static const HARGA_SAMPAH = '/harga-sampah';
+  static const HARGA_SAMPAH = '/harga-sampah';
   static const RIWAYAT = '/riwayat';
   static const TRANSAKSI = '/transaksi';
   static const JADWAL = '/jadwal';
   static const PENGAMBILAN = '/pengambilan';
   static const BANTUAN = '/bantuan';
   static const STATISTIKNASABAH = '/statistiknasabah';
-  
   static const PEMAYARAN = '/pemayaran';
   static const PAYMENTPAGE = '/paymentpage';
 }
