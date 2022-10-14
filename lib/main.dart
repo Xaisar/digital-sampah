ca084e9091934d1bcd71bde98ece9cf86061ee5d
ajil23 <azrilpraya@gmail.com>
2022-10-14 20:30:23 +0800

    ajril

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:digital_sampah/app/modules/home_page/views/home_page_view.dart';
+import 'package:digital_sampah/app/modules/home_petugas/views/home_petugas_view.dart';
 import 'package:digital_sampah/app/modules/pemayaran/views/pemayaran_view.dart';
 import 'package:digital_sampah/app/modules/transaksi/views/transaksi_view.dart';
 import 'package:digital_sampah/widgets/splash.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PemayaranView(),
+      home: HomePetugasView(),
     );
     return FutureBuilder(
       future: Future.delayed(Duration(seconds: 5)),
