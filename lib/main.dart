5bfc5fb3b54d8f45848217325f4925962fab61eb
ajil23 <azrilpraya@gmail.com>
2022-10-14 15:33:40 +0800

    ajril

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:digital_sampah/app/modules/transaksi/views/transaksi_view.dart';
 import 'package:digital_sampah/widgets/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'app/modules/transaksi/views/transaksi_view.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
