cafcb5d89f0ccae91e227d45ed893de7b5ed0d4b
Arilex <110915525+Xaisar@users.noreply.github.com>
2022-12-08 10:37:00 +0700

    push

--- lib/app/modules/home_page/controllers/home_page_controller.dart
+++ lib/app/modules/home_page/controllers/home_page_controller.dart
@@ -1,7 +1,31 @@
 import 'package:get/get.dart';
 
 class HomePageController extends GetxController {
-  final count = 0.obs;
+  final List<Map<String, dynamic>> isinasabah = [
+    {
+      'id': '1',
+      'userId': '1',
+      "nama": 'Nada Celia Sinka Ines',
+      'tempatLahir': 'Banyuwangi',
+      'tanggalLahir': '2 Februari 2002 ',
+      'alamat': 'Rogojampi',
+      'dawis': '001',
+      'tanggalMasuk': '3 Mei 2021',
+      'saldo': 0
+    },
+    {
+      'id': '2',
+      'userId': '2',
+      "nama": 'Rizal Andriansyah',
+      'tempatLahir': 'Banyuwangi',
+      'tanggalLahir': '1 Juni 2003',
+      'alamat': 'Rogojampi',
+      'dawis': '001',
+      'tanggalMasuk': '15 Maret 2022',
+      'saldo': 0
+    }
+  ];
+
   @override
   void onInit() {
     super.onInit();
@@ -11,8 +35,4 @@ class HomePageController extends GetxController {
   void onReady() {
     super.onReady();
   }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
